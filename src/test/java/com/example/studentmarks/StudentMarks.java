612e91bf37b7c711210c08f646ec2121fe080707
jasminshaik6866 <jasminshaik6866@gmail.com>
2024-06-17 22:41:29 +0530

    create new repo

---
+++ src/test/java/com/example/studentmarks/StudentMarks.java
@@ -0,0 +1,11 @@
+package com.example.studentmarks;
+
+public class StudentMarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the marks");
+
+	}
+
+}
